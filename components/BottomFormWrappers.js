4fa0aef1655f3f19742e8d965c6849558226885d
Peter Forbes <66070260+pjf1822@users.noreply.github.com>
2023-12-19 18:28:43 -0500

    animation progress

--- components/BottomFormWrappers.js
+++ components/BottomFormWrappers.js
@@ -1,5 +1,5 @@
-import { View, StyleSheet } from "react-native";
-import React from "react";
+import { View, StyleSheet, Animated, Easing } from "react-native";
+import React, { useEffect, useRef } from "react";
 import DescriptionForm from "./DescriptionForm";
 import BottomForm from "./BottomForm";
 import {
@@ -15,21 +15,64 @@ const BottomFormWrappers = ({
   selectedId,
   setSelectedId,
 }) => {
+  const slideAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    const slideValue = selectedId ? 0 : 1;
+    Animated.timing(slideAnim, {
+      toValue: slideValue,
+      duration: 150,
+      easing: Easing.ease,
+      useNativeDriver: false,
+    }).start();
+  }, [selectedId, slideAnim]);
+
   return (
     <View style={styles.formsWrapper}>
-      {selectedId ? (
+      <Animated.View
+        style={[
+          styles.singleForm,
+
+          {
+            transform: [
+              {
+                translateY: slideAnim.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [hp("2%"), hp("90%")],
+                }),
+              },
+            ],
+          },
+        ]}
+      >
         <BottomForm
           selectedId={selectedId}
           getAllAddresses={getAllAddresses}
           setSelectedId={setSelectedId}
         />
-      ) : (
+      </Animated.View>
+
+      <Animated.View
+        style={[
+          styles.singleForm,
+          {
+            transform: [
+              {
+                translateY: slideAnim.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [hp("50%"), hp("-14")],
+                }),
+              },
+            ],
+          },
+        ]}
+      >
         <DescriptionForm
           getAllAddresses={getAllAddresses}
           setCoordinates={setCoordinates}
           setZoom={setZoom}
         />
-      )}
+      </Animated.View>
     </View>
   );
 };
@@ -47,5 +90,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     zIndex: 99,
     backgroundColor: colors.tan,
+    maxHeight: hp("22%"),
   },
+  singleForm: {},
 });
